2e5fc62b255f47c17502abc410499dcea74cb43d
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-01-13 21:27:40 +0530

    Add files via upload

---
+++ angle of triangle.cpp
@@ -0,0 +1,13 @@
+//C program to find all angles of a triangle if two angles are given.
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter two angles of triangle: ");
+    scanf("%d%d", &a, &b);
+    c = 180 - (a + b);
+    printf("Third angle of the triangle = %d", c);
+
+    return 0;
+}
