aaba1053a0fc3db1149fa21660f765c67dec1681
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-01-13 21:49:14 +0530

    Add files via upload

---
+++ perfect no.cpp
@@ -0,0 +1,27 @@
+// C program to print all Perfect numbers between 1 to n AND Check a given number is Perfect numbers or not.
+#include <stdio.h>
+int main()
+{
+    int i, j, end, sum;
+    printf("Enter upper limit: ");
+    scanf("%d", &end);
+
+    printf("All Perfect numbers between 1 to %d:\n", end);
+    for(i=1; i<=end; i++)
+    {
+        sum = 0;
+        for(j=1; j<i; j++)
+        {
+            if(i % j == 0)
+            {
+                sum += j;
+            }
+        }
+        if(sum == i)
+        {
+            printf("%d, ", i);
+        }
+    }
+
+    return 0;
+}
