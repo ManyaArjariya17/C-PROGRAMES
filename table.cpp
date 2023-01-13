812c982b1ca72a9b698cbd9882f7aaf56d464848
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-01-13 21:39:10 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,12 @@
+//C program to print multiplication table of a number
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf("%d * %d = %d \n", n, i, n * i);
+  }
+  return 0;
+}
+
