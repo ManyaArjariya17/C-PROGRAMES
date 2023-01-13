7b384aacb45184aca04f7fc3a9b5a3d5de91eadc
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-01-13 21:47:42 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,20 @@
+// C program to print Armstrong numbers from 1 to n AND Check a given number is Armstrong numbers or not.
+#include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("armstrong  number ");    
+else    
+printf("not armstrong number");    
+return 0;  
+}   
