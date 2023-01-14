3d4ba63f80ac65059889545576d4ad7b0955f8fa
ManyaArjariya17 <122359135+ManyaArjariya17@users.noreply.github.com>
2023-01-14 20:03:51 +0530

    Add files via upload

---
+++ delete element array.cpp
@@ -0,0 +1,36 @@
+// C program to delete an element in array at specified position
+#include <stdio.h>
+int main(){
+  int arr1[50],i,pos,n;
+  
+       printf("\n\nDelete an element at desired position from an array :\n");
+       printf("---------------------------------------------------------\n");  
+  
+       printf("Input the size of array : ");
+       scanf("%d", &n);
+       printf("Input %d elements in the array in ascending order:\n",n);
+       for(i=0;i<n;i++)
+            {
+	      printf("element - %d : ",i);
+	      scanf("%d",&arr1[i]);
+	    }
+
+  printf("\nInput the position where to delete: ");
+  scanf("%d",&pos);
+  i=0;
+  while(i!=pos-1)
+            i++;
+  while(i<n){
+            arr1[i]=arr1[i+1];
+            i++;
+  }
+  n--;
+   printf("\nThe new list is : "); 
+  for(i=0;i<n;i++)
+        {
+		   printf("  %d",arr1[i]);
+		}  
+            printf("\n\n");
+  return 0;          
+
+}
